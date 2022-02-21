7b1ba1c73b7030b03dc4115f87f7e05d47998b34
Oliver Schwill <oliver.schwill@smartcommerce.de>
2022-02-21 09:58:10 +0100

    Bestellung hinzugefügt

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -4,22 +4,35 @@ namespace App\Controller;
 
 use App\Entity\Meals;
 use App\Entity\Order;
+use App\Repository\OrderRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
-    #[Route('/order', name: 'order')]
-    public function index(): Response
+    private ManagerRegistry $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
     {
+        $this->doctrine = $doctrine;
+    }
+
+    #[Route('/order', name: 'order')]
+    public function index(OrderRepository $orderRep): Response
+    {
+        $orders = $orderRep->findBy(
+            ['tisch' => 'tisch1']
+        );
+
         return $this->render('order/index.html.twig', [
-            'controller_name' => 'OrderController',
+            'orders' => $orders
         ]);
     }
 
-    #[Route('/order/{id}', name: 'order')]
-    public function order(Meals $meals)
+    #[Route('/order/{id}', name: 'orderMeal')]
+    public function order(Meals $meals, ManagerRegistry $doctrine)
     {
         $order = new Order();
         $order->setTisch("tisch1");
@@ -28,6 +41,27 @@ class OrderController extends AbstractController
         $order->setPrice($meals->getPrice());
         $order->setStatus("offen");
 
-        
+        // entityManager
+        $em = $this->doctrine->getManager();
+        $em->persist($order);
+        $em->flush();
+
+        $this->addFlash('order', $order->getName() . ' wurde zur Bestellung hinzugefügt!');
+
+        return $this->redirect($this->generateUrl('menu'));
+    }
+
+    #[Route('/status/{id}, {status}', name: 'status')]
+    public function status($id, $status)
+    {
+        $em = $this->doctrine->getManager();
+        // Wir suchen mit der übergebenen Id den Datensatz aus dem Order Repository
+        $order = $em->getRepository(Order::class)->find($id); 
+
+        // Wir setzten den neuen Status
+        $order->setStatus($status);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('order'));
     }
 }
